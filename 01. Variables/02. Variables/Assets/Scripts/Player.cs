39fc940651060836b05e5023c95ae8a31c9c3c9e
jtarrier-sc <jtarrier@yahoo.com>
2020-07-11 04:24:47 +0800

    Added '<summary> ... </summary>' block to 'Player' C# script

--- 01. Variables/02. Variables/Assets/Scripts/Player.cs
+++ 01. Variables/02. Variables/Assets/Scripts/Player.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+	/// <summary>
+	/// To create variables there are 3 required components with an optional 4th:
+	/// 1. public or private reference ("Who can access this information?")
+	/// 	public = everyone can access the variable, can be seen and manipulated in the Unity editor
+	/// 	private = only this script has control of the variable.
+	/// 2. data type ("What kind of variable is this?")
+	/// 	3 common types:
+	///			string (characters of text e.g. a name, a sentence)
+	///			bool (logical true or false)
+	///			float (decimal number)
+	///			int (whole number)
+	///	3. name
+	/// 4. value (optional)
+	/// </summary>
+	
     // Start is called before the first frame update
     void Start()
     {
