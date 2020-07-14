4697a91cd255bd9a661291c394b868944f75558c
jtarrier-sc <jtarrier@yahoo.com>
2020-07-14 10:31:35 +0800

    'Player' C# script - Added challenge outline.

--- 01. Variables/04. Challenge Master Variables Part 01/Assets/Scripts/Player.cs
+++ 01. Variables/04. Challenge Master Variables Part 01/Assets/Scripts/Player.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // create a variable to store your name
+    // create a variable to store your age
+    // create a variable to store your speed
+    // create a health variable
+    // create a score variable
+    // create a speed variable
+    // create a variable to check if all keys were collected
+    // create a variable for ammo count
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // print out variables with appropriate descriptive text
     }
 
     // Update is called once per frame
