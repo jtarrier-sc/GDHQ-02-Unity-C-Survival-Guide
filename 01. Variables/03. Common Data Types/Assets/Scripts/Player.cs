83704062425a9e35bc06156ae0ce5196812d8121
jtarrier-sc <jtarrier@yahoo.com>
2020-07-14 09:53:47 +0800

    'Player' C# script - Added example variables of C# basic data types and Unity data types. Assigned values & output of 'Main Camera' Transform values

--- 01. Variables/03. Common Data Types/Assets/Scripts/Player.cs
+++ 01. Variables/03. Common Data Types/Assets/Scripts/Player.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // C# basic data types (examples)
+    public int score;
+    public float speed = 5.5f; // the 'f' suffix is required
+
+    // Unity data types (examples)
+    public GameObject player;
+    public Animator anim;
+    public Transform myTransform; // value assigned by dragging & dropping 'Main Camera' Transform in Unity editor
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Main camera 'Transform' settings:");
+        Debug.Log("x = " + myTransform.position.x);
+        Debug.Log("y = " + myTransform.position.y);
+        Debug.Log("z = " + myTransform.position.z);
     }
 
     // Update is called once per frame
