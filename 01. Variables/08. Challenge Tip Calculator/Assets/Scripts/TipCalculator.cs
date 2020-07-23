0f93a7acff4eb206edf90ff0afac2a80e67fd0bc
jtarrier-sc <jtarrier@yahoo.com>
2020-07-23 09:12:16 +0800

    'TipCalculator' C# script - Added challenge outline.

--- 01. Variables/08. Challenge Tip Calculator/Assets/Scripts/TipCalculator.cs
+++ 01. Variables/08. Challenge Tip Calculator/Assets/Scripts/TipCalculator.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class TipCalculator : MonoBehaviour
 {
+    /// <summary>
+    /// Bill is 40 dollars
+    /// Tip is 20% or based on what the user wants
+    /// Calculate total amount
+    /// </summary>
+    
+
     // Start is called before the first frame update
     void Start()
     {
+        // Print message in the format "Your bill is: ??? and your tip amount is: ??? so you owe: totalAmount"
         
     }
 
